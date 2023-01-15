b949b5f3062feba745ea71fa6fc460555a5a8f8d
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-01-15 08:21:04 +0800

    Write an I/O code

--- Personal/projbasics/src/main.rs
+++ Personal/projbasics/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input: String = String::new();
+    println!("Say something");
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("You said {}", input);
+        }
+        Err(e) => {
+            println!("Something went wrong {}", e);
+        }
+    }
 }
