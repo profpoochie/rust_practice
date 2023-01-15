d895669f255b9dfd69c450a826838f19bec47c37
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-01-15 21:18:18 +0800

    Practice variables

--- Personal/lang_basics/variable_eg/src/main.rs
+++ Personal/lang_basics/variable_eg/src/main.rs
@@ -1,3 +1,18 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
 fn main() {
-    println!("Hello, world!");
+    let name: &str = "Alex";
+    let mut age: i32 = 32;
+    let amount: i64 = 5115112120202;
+
+    age = 43;
+    println!("age {}", age);
+
+    let color: &str = "blue";
+    let color: i32 = 86;
+
+    println!("{}", color);
+
+    let (a, b, c) = (43, 85, "red");
 }
