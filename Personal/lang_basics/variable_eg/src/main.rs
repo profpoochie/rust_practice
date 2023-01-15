af55d98ee951674ec12b86a8d61031f7df7c737f
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-01-15 21:53:45 +0800

    Trying string methods

--- Personal/lang_basics/variable_eg/src/main.rs
+++ Personal/lang_basics/variable_eg/src/main.rs
@@ -2,7 +2,7 @@
 #[allow(unused_assignments)]
 
 fn main() {
-    let name: &str = "Alex";
+    /*     let name: &str = "Alex";
     let mut age: i32 = 32;
     let amount: i64 = 5115112120202;
 
@@ -28,5 +28,29 @@ fn main() {
 
     let char1 = 'A';
     let smiley_face = '\u{1f601}';
-    println!("characters {} and {}", char1, smiley_face);
+    println!("characters {} and {}", char1, smiley_face); */
+
+    // String slices immutable
+    let cat: &'static str = "Fluffy";
+    println!("{}", cat);
+
+    // String objects mutable
+    let dog = String::new();
+    let mut dog = String::from("Max");
+    println!("{}", dog);
+
+    // format! macro
+    let owner = format!("Hi I'm {} the owner of {}", "Mark", dog);
+    println!("{}", owner);
+
+    // length of the string
+    println!("length of the string {}", dog.len());
+
+    // push & push string
+    dog.push(' ');
+    dog.push_str("the dog");
+    println!("{}", dog);
+
+    let new_dog = dog.replace("the", "is my");
+    println!("{}", new_dog);
 }
