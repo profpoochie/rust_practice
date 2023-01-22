d77565b3dbb0266b976b5b3108194e54a4089887
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-01-22 23:59:28 +0800

    Structures example

--- structure_example/src/main.rs
+++ structure_example/src/main.rs
@@ -1,3 +1,35 @@
+use std::fmt::format;
+
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
 fn main() {
-    println!("Hello, world!");
+    let emp = Employee {
+        name: String::from("John"),
+        company: String::from("Google"),
+        age: 35
+    };
+
+    println!("{:?}",emp);
+    println!("{}", emp.name);
+    println!("{}", emp.fn_details());
+    println!("{}", Employee::static_fn_details());
 }
+
+#[derive(Debug)]
+struct Employee {
+    name: String,
+    company: String,
+    age: u32,
+}
+
+impl Employee {
+    fn fn_details(&self) -> String {
+        format!("Name: {}, age: {}, company: {} ", &self.name, &self.age, &self.company)
+    }
+
+    fn static_fn_details() -> String{
+        String::from("Details of a person")
+    }
+}
+
